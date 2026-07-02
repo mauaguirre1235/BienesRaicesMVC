1c5981b55837f8e1b55e6f33984a63179a0163e5
Mauricio <maau.cursos@gmail.com>
2026-07-02 12:58:50 -0600

    Creando las URL'S para las paginas

--- public/index.php
+++ public/index.php
@@ -5,10 +5,11 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router; 
 use Controllers\PropiedadController; 
 use Controllers\VendedorController;
+use Controllers\PaginasController; 
 
 $router = new Router(); 
 
-  
+// ZONA PRIVADA
 $router->get('/admin', [PropiedadController::class, 'index']); 
 $router->get('/propiedades/crear', [PropiedadController::class, 'crear']); 
 $router->post('/propiedades/crear', [PropiedadController::class, 'crear']); 
@@ -22,5 +23,16 @@ $router->post('/vendedores/crear', [VendedorController::class, 'crear']);
 $router->get('/vendedores/actualizar', [VendedorController::class, 'actualizar']); 
 $router->post('/vendedores/actualizar', [VendedorController::class, 'actualizar']); 
 $router->post('/vendedores/eliminar', [VendedorController::class, 'eliminar']); 
+
+// ZONA PUBLICA
+$router->get('/', [PaginasController::class, 'index']); 
+$router->get('/nosotros', [PaginasController::class, 'nosotros']); 
+$router->get('/propiedades', [PaginasController::class, 'propiedades']); 
+$router->get('/propiedad', [PaginasController::class, 'propiedad']); 
+$router->get('/blog', [PaginasController::class, 'blog']); 
+$router->get('/entrada', [PaginasController::class, 'entrada']); 
+$router->get('/contacto', [PaginasController::class, 'contacto']); 
+$router->post('/contacto', [PaginasController::class, 'contacto']); 
+
 
 $router->comprobarRutas();
