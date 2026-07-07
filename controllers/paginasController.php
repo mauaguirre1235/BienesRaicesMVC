5b06b5aa3e2d45c54895235cfe33924e15a93027
Mauricio <maau.cursos@gmail.com>
2026-07-07 09:50:04 -0600

    Enviar un email con los datos del formulario

--- controllers/paginasController.php
+++ controllers/paginasController.php
@@ -54,6 +54,9 @@ class PaginasController
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
+
+            $respuestas = $_POST['contacto'];
+            
             // crear una instancia de PHPMailer
             $mail = new PHPMailer();
 
@@ -75,7 +78,18 @@ class PaginasController
             $mail->CharSet = 'UTF-8';
 
             // definir el contenido 
-            $contenido = '<html><p>Tienes un nuevo mensaje</p></html>';
+            $contenido = '<html>';
+            $contenido .= '<p>Tienes un nuevo mensaje</p>';
+            $contenido .= '<p>Nombre:  ' . $respuestas['nombre'] . '</p>';
+            $contenido .= '<p>Email:  ' . $respuestas['email'] . '</p>';
+            $contenido .= '<p>Telefono:  ' . $respuestas['telefono'] . '</p>';
+            $contenido .= '<p>Mensaje:  ' . $respuestas['mensaje'] . '</p>';
+            $contenido .= '<p>Vende o Compra: ' . $respuestas['tipo'] . '</p>';
+            $contenido .= '<p>Precio o Presupuesto:  $' . $respuestas['precio'] . '</p>';
+            $contenido .= '<p>Prefieres ser contactado por:  ' . $respuestas['contacto'] . '</p>';
+            $contenido .= '<p>Fecha:  ' . $respuestas['fecha'] . '</p>';
+            $contenido .= '<p>Hora:  ' . $respuestas['hora'] . '</p>';
+            $contenido .= '</html>';
 
             $mail->Body = $contenido;
             $mail->AltBody = 'Esto es texto alternativo sin HTML';
--- views/paginas/contacto.php
+++ views/paginas/contacto.php
@@ -1,68 +1,70 @@
 <main class="contenedor">
-        <h1>Contacto</h1>
+    <h1>Contacto</h1>
 
-        <picture>
-            <source srcset="build/img/destacada3.webp" type="image/webp">
-            <source srcset="build/img/destacada3.jpg" type="image/jpeg">
-            <img loading="lazy" src="build/img/destacada3.jpg" alt="Imagen contacto">
-        </picture>
+    <picture>
+        <source srcset="build/img/destacada3.webp" type="image/webp">
+        <source srcset="build/img/destacada3.jpg" type="image/jpeg">
+        <img loading="lazy" src="build/img/destacada3.jpg" alt="Imagen contacto">
+    </picture>
 
-        <h2> LLene el Formulario de Contacto </h2>
+    <h2> LLene el Formulario de Contacto </h2>
 
-        <form class="formulario" action="/contacto" method="POST">
-            <fieldset>
-                <legend>Informacion Personal</legend>
-                <label for="nombre"> Nombre:</label>
-                <input id="nombre" type="text" placeholder="Tu nombre" name="contacto[nombre]" required>
+    <form class="formulario" action="/contacto" method="POST">
+        <fieldset>
+            <legend>Informacion Personal</legend>
+            <label for="nombre"> Nombre:</label>
+            <input id="nombre" type="text" placeholder="Tu nombre" name="contacto[nombre]" >
 
-                 <label for="email">E-mail:</label>
-                <input id="email" type="email" placeholder="Tu email" name="contacto[email]" required>
+            <label for="email">E-mail:</label>
+            <input id="email" type="email" placeholder="Tu email" name="contacto[email]" >
 
-                
-                 <label for="telefono">Telefono:</label>
-                <input id="telefono" type="tel" placeholder="Telefono" name="contacto[telefono]" >
 
-                
-                 <label for="mensaje" >Mensaje:</label>
-                <textarea id="mensaje" name="contacto[mensaje]" required> </textarea>
-            </fieldset>
+            <label for="telefono">Telefono:</label>
+            <input id="telefono" type="tel" placeholder="Telefono" name="contacto[telefono]">
 
-            <fieldset>
-                <legend>Informacion Sobre la propiedad</legend>
-                <label for="opciones">Vente o Compra:</label>
-                <select id="opciones" name="contacto[tipo]" required>
-                    <option value="" disabled selected>--Seleccione--</option>
-                    <option value="Compra">Compra</option>
-                    <option value="Vende">Vende</option>
-                </select>
 
-                 <label for="presupuesto">Precio o Presupuesto:</label>
-                <input id="presupuesto" type="number" placeholder="presupuesto" name="contacto[precio]" required>
-            </fieldset>
+            <label for="mensaje">Mensaje:</label>
+            <textarea id="mensaje" name="contacto[mensaje]" > </textarea>
+        </fieldset>
 
-            <fieldset>
-                <legend>Informacion sobre la propiedad</legend>
-                
-                <p>Como desea ser contactado</p>
+        <fieldset>
+            <legend>Informacion Sobre la propiedad</legend>
+            <label for="opciones">Vente o Compra:</label>
+            <select id="opciones" name="contacto[tipo]" >
+                <option value="" disabled selected>--Seleccione--</option>
+                <option value="Compra">Compra</option>
+                <option value="Vende">Vende</option>
+            </select>
 
-                <div class="forma-contacto">
-                    <label for="contactar-telefono">Telefono</label>
-                    <input name="contacto" id="contactar-telefono" type="radio" value="telefono" name="contacto[contacto]" required>
+            <label for="presupuesto">Precio o Presupuesto:</label>
+            <input id="presupuesto" type="number" placeholder="presupuesto" name="contacto[precio]" >
+        </fieldset>
 
-                    <label for="contactar-email">E-mail</label>
-                    <input name="contacto" id="contactar-email" type="radio" value="email" name="contacto[contacto]" required>
-                </div>
+        <fieldset>
+            <legend>Informacion sobre la propiedad</legend>
 
-                <p>Si eligio telefono, elija la fecha y la hora para ser contactado</p>
-                <label for="fecha">Fecha:</label>
-                <input id="fecha" type="date" name="contacto[fecha]" >
+            <p>Como desea ser contactado</p>
 
-                <label for="hora">Hora:</label>
-                <input id="hora" type="time" min="09:00" max="18:00" name="contacto[hora]">
+            <div class="forma-contacto">
+                <label for="contactar-telefono">Telefono</label>
+                <input  id="contactar-telefono" type="radio" value="telefono" name="contacto[contacto]"
+                    >
 
-            </fieldset> 
+                <label for="contactar-email">E-mail</label>
+                <input  id="contactar-email" type="radio" value="email" name="contacto[contacto]"
+                >
+            </div>
 
-            <input type="submit" value="Enviar" class="boton-verde">
+            <p>Si eligio telefono, elija la fecha y la hora para ser contactado</p>
+            <label for="fecha">Fecha:</label>
+            <input id="fecha" type="date" name="contacto[fecha]">
 
-        </form>
-    </main>
+            <label for="hora">Hora:</label>
+            <input id="hora" type="time" min="09:00" max="18:00" name="contacto[hora]">
+
+        </fieldset>
+
+        <input type="submit" value="Enviar" class="boton-verde">
+
+    </form>
+</main>
